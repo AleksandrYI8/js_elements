6ec1ab8f129590ddea193839585a66f0eed40031
yunus_ibadov <ibadovunus55@gmail.com>
2023-09-25 15:08:43 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+
+/* for(let i = 1; i <= 10; i++){
+
+let div = document.createElement('div')
+let h5 = document.createElement('h5')
+let p = document.createElement('p')
+let button = document.createElement('button')
+
+
+
+    div.classList.add('div')
+
+h5.classList.add('h5')
+h5.innerHTML = "lorem grammar"
+
+p.classList.add('p')
+p.innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita eum explicabo nisi repellendus ipsam voluptates ."
+
+button.classList.add('button')
+button.innerHTML = "lorem"
+
+let body = document.body
+
+body.append(div)
+div.append(h5, p, button)
+} */
+
+
+
